9eb337e1dd0f05f71e469d3bb407167716e28818
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2025-06-24 09:10:45 -0300

    ajuste styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,20 +8,20 @@ export default function Home() {
                 alt=""
                 className="fixed top-0 right-0 z-0 pointer-events-none opacity-4"
             />
-            <h1 className="text-3xl lg:text-4xl font-bold lg:mb-[-5px] m-10">Sejam bem-vindos ao meu portfólio!</h1>
+            <h1 className="text-3xl lg:text-4xl font-bold lg:mb-[-5px] m-6">Sejam bem-vindos ao meu portfólio!</h1>
 
-            <div className="flex flex-col lg:flex-row">
-                <div className="lg:p-10 flex flex-col items-center justify-center lg:items-start lg:justify-start">
-                    <img src="Rafa.jpg" alt="" className="w-100 rounded-lg mb-2" />
-                    <a href="/Curriculo.pdf" download="Curriculo-Rafael-Thomaz.pdf" className="w-100">
-                        <Button className="w-100 cursor-pointer">
+            <div className="flex flex-col lg:flex-row ">
+                <div className="lg:p-10  flex flex-col items-center justify-center lg:items-start lg:justify-start ">
+                    <img src="Rafa.jpg" alt="" className="w-95 rounded-lg mb-2" />
+                    <a href="/Curriculo.pdf" download="Curriculo-Rafael-Thomaz.pdf" className=" flex flex-col  items-center justify-center  w-100">
+                        <Button className="w-95 cursor-pointer">
                             Baixar Currículo
                         </Button>
                     </a>
 
                 </div>
 
-                <div className="p-10">
+                <div className="p-8">
                     <h4 className="lg:text-2xl text-lg font-medium">Meu nome é Rafael Henrique Thomaz,</h4>
                     <p className="lg:text-lg">Sou desenvolvedor com foco em Front-End, apaixonado por tecnologia e sempre em busca de evolução constante na área da programação. Desde 2022, venho estudando e aprimorando meus conhecimentos no desenvolvimento web e adquirindo experiência prática em diferentes projetos e tecnologias.</p>
                     <p className="lg:text-lg mt-5">Já trabalhei com ferramentas e linguagens como HTML, CSS, JavaScript, SQL, Firebase, C#, ReactJS, Next.js e Tailwind CSS, sempre buscando criar interfaces funcionais, modernas e com boa experiência para o usuário.</p>
