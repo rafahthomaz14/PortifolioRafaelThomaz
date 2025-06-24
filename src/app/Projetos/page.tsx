b218fb7dfe652fc344a96575bf24ee7edc9f1490
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2025-06-24 16:22:49 -0300

    add projetos

--- src/app/Projetos/page.tsx
+++ src/app/Projetos/page.tsx
@@ -1,5 +1,49 @@
+import { Button } from "@/components/ui/button"
+
+
 export default function Projetos() {
     return (
-        <h1 className="text-3xl lg:text-4xl font-bold p-5">Meus Projetos</h1>
+        <>
+            <img
+                src="logo.png"
+                alt=""
+                className="fixed top-0 right-0 z-0 pointer-events-none opacity-4"
+            />
+            <h1 className="text-3xl lg:text-4xl font-bold p-5">Meus Projetos</h1>
+
+            <div className="p-4 lg:p-10 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
+                    <img src="./canteen.jpg" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
+                    <div className="flex w-[100%] justify-between">
+                        <Button className=" w-[49%] mt-2">Sobre</Button>
+                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/School_Canteen" target="_blank">GitHub</a></Button>
+                    </div>
+                </div>
+
+                <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
+                    <img src="./mre.png" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
+                    <div className="flex w-[100%] justify-between">
+                        <Button className=" w-[49%] mt-2">Sobre</Button>
+                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/MRE_Solucoes" target="_blank">GitHub</a></Button>
+                    </div>
+                </div>
+
+
+                <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
+                    <img src="./plantech.jpg" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
+                    <div className="flex w-[100%] justify-between">
+                        <Button className=" w-[49%] mt-2">Sobre</Button>
+                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/Projeto_Plantech" target="_blank">GitHub</a></Button>
+                    </div>
+                </div>
+
+
+
+            </div>
+
+
+
+        </>
+
     )
 }
