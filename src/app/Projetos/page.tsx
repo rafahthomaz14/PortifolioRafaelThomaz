1518538740147e0fb0b32672b29eb77ea6605566
Rafael Thomaz <rafael.hthomaz123@gmail.com>
2025-06-24 17:05:15 -0300

    ajuste button

--- src/app/Projetos/page.tsx
+++ src/app/Projetos/page.tsx
@@ -15,16 +15,28 @@ export default function Projetos() {
                 <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
                     <img src="./canteen.jpg" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
                     <div className="flex w-[100%] justify-between">
-                        <Button className=" w-[49%] mt-2">Sobre</Button>
-                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/School_Canteen" target="_blank">GitHub</a></Button>
+                        <Button className=" cursor-pointer w-[49%] mt-2">Sobre</Button>
+                        <a
+                            href="https://github.com/rafahthomaz14/School_Canteen"
+                            target="_blank"
+                            className="w-[49%] mt-2 inline-block"
+                        >
+                            <Button className=" cursor-pointer w-full">GitHub</Button>
+                        </a>
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
                     <img src="./mre.png" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
                     <div className="flex w-[100%] justify-between">
-                        <Button className=" w-[49%] mt-2">Sobre</Button>
-                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/MRE_Solucoes" target="_blank">GitHub</a></Button>
+                        <Button className="cursor-pointer w-[49%] mt-2">Sobre</Button>
+                        <a
+                            href="https://github.com/rafahthomaz14/MRE_Solucoes"
+                            target="_blank"
+                            className="w-[49%] mt-2 inline-block"
+                        >
+                            <Button className="cursor-pointer w-full">GitHub</Button>
+                        </a>
                     </div>
                 </div>
 
@@ -32,16 +44,28 @@ export default function Projetos() {
                 <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
                     <img src="./plantech.jpg" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
                     <div className="flex w-[100%] justify-between">
-                        <Button className=" w-[49%] mt-2">Sobre</Button>
-                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/Projeto_Plantech" target="_blank">GitHub</a></Button>
+                        <Button className=" cursor-pointer w-[49%] mt-2">Sobre</Button>
+                        <a
+                            href="https://github.com/rafahthomaz14/Projeto_Plantech"
+                            target="_blank"
+                            className="w-[49%] mt-2 inline-block"
+                        >
+                            <Button className="cursor-pointer w-full">GitHub</Button>
+                        </a>
                     </div>
                 </div>
 
                 <div className="flex flex-col items-center bg-gray-200 p-3 rounded-2xl ">
                     <img src="./portifolio1.png" className="w-full h-full object-cover rounded-md" alt="Imagem 1" />
                     <div className="flex w-[100%] justify-between">
-                        <Button className=" w-[49%] mt-2">Sobre</Button>
-                        <Button className=" w-[49%] mt-2"><a href="https://github.com/rafahthomaz14/Portifolio" target="_blank">GitHub</a></Button>
+                        <Button className=" cursor-pointer w-[49%] mt-2">Sobre</Button>
+                        <a
+                            href="https://github.com/rafahthomaz14/Portifolio"
+                            target="_blank"
+                            className="w-[49%] mt-2 inline-block"
+                        >
+                            <Button className="cursor-pointer w-full">GitHub</Button>
+                        </a>
                     </div>
                 </div>
 
